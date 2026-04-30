e4c2213381ff9a11b2902a52f1ea205557a496d8
Lucas Barbosa <flucas.dev@gmail.com>
2026-04-30 18:13:51 -0300

    initial structure

---
+++ src/main/java/tecnologias_emergentes/TecnologiasEmergentesApplication.java
@@ -0,0 +1,13 @@
+package tecnologias_emergentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnologiasEmergentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnologiasEmergentesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tecnologias_emergentes/TecnologiasEmergentesApplicationTests.java
@@ -0,0 +1,13 @@
+package tecnologias_emergentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnologiasEmergentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
