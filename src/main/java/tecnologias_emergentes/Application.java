b4b13dc5711dfed0efbd59f5d5beaeb5e69d4299
Lucas Barbosa <flucas.dev@gmail.com>
2026-05-14 19:18:39 -0300

    Docs added to repository and renamed to Application the main class

--- src/main/java/tecnologias_emergentes/Application.java
+++ src/main/java/tecnologias_emergentes/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TecnologiasEmergentesApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TecnologiasEmergentesApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/tecnologias_emergentes/ApplicationTests.java
+++ src/test/java/tecnologias_emergentes/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TecnologiasEmergentesApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
